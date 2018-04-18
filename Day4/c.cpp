7af3c2d86763212cae6078031273b7e9f378761f
Mac <Mac@MacBook-Air.local>
2018-04-18 13:44:31 +0800

    2018.4.18

--- Day4/c.cpp
+++ Day4/c.cpp
@@ -2,38 +2,33 @@
 #include <cmath>
 #define INF 1000000000
 
-int cur=-1,n,t,s,e,I,f[105][105][25],steps[1000005];
+int cur=-1,n,t,s,e,I,f[1005][1005][25],steps[1000005];
 
 int DFS(int s,int e,int n)
 {
     int step=steps[n];
 
     if (f[s][e][step]!=INF || n==1)
-    {
-        //printf("%d %d %d F:%d\n",s,e,n,f[s][e][step]);
         return f[s][e][step];
-    }
     if (n&1)
     {
         int ret=INF;
         for (int i=1,cur;i<=I;i++)
             if (i!=s && i!=e && (cur=DFS(s,i,n-1))+f[s][i][0]<ret) ret=cur;
-        //printf("%d %d %d F:%d\n",s,e,n,ret);
         return ret;
     }
     else
     {
         for (int i=1,cur;i<=I;i++)
             if (i!=s && i!=e && (cur=DFS(s,i,n/2)+DFS(i,e,n/2))<f[s][e][step]) f[s][e][step]=cur;
-        //printf("%d %d %d F:%d\n",s,e,n,f[s][e][step]);
         return f[s][e][step];
     }
 }
 
 int main()
 {
-    for (int i=0;i<105;i++)
-        for (int j=0;j<105;j++)
+    for (int i=0;i<1005;i++)
+        for (int j=0;j<1005;j++)
             for (int k=0;k<25;k++)
                 f[i][j][k]=INF;
 
