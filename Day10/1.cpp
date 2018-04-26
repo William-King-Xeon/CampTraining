d426e014956bd5d7aa2f221dd78350396d6183eb
Mac <Mac@MacBook-Air.local>
2018-04-26 19:27:40 +0800

    2018.4.26

--- Day10/1.cpp
+++ Day10/1.cpp
@@ -2,7 +2,13 @@
 
 int n,father[100005],cnt[100005],q,U,V,
     from[100005],to[100005];
-char cmd;
+char cmd[1];
+
+int Compress(int x)
+{
+    if (cnt[x]<=1) return x;
+    return father[x]=Compress(father[x]);
+}
 
 void Add(int u,int v)
 {
@@ -10,11 +16,7 @@ void Add(int u,int v)
     if (u<v) { int t=u;u=v;v=t; }
     cnt[u]++;
     from[u]=U,to[u]=V;
-    if (cnt[u]>1)
-    {
-        while (cnt[u]>1) u=father[u];
-        father[u]=u;
-    }
+    Compress(u);
     Add(father[u],v);
     return;
 }
@@ -24,16 +26,17 @@ int main()
     freopen("2490.in","r",stdin);
     freopen("2490.out","w",stdout);
 
-    scanf("%d\n",&n);
+    scanf("%d",&n);
+    printf("%d\n",n);
 
     for (int i=1;i<n;i++) scanf("%d",&father[i]);
 
-    scanf("\n%d",&q);
+    scanf("%d",&q);
 
     while (q--)
     {
-        scanf("\n%c",&cmd);
-        if (cmd=='R')
+        scanf("%s",cmd);
+        if (cmd[0]=='R')
         {
             scanf("%d%d",&U,&V);
             if (U>V) { int t=U;U=V;V=t; }
@@ -43,7 +46,7 @@ int main()
             scanf("%d",&V);
             if (cnt[V]==0) printf("Not yet\n");
                 else if (cnt[V]==1) printf("%d %d\n",from[V],to[V]);
-                    else printf("Mant times\n");
+                    else printf("Many times\n");
         }
     }
 
